dff9b613e248bfda271d5343b7d9b8a724d70755
Joseph Bacal <bacalj@gmail.com>
2021-12-20 21:25:31 -0500

    updating map as we go a little

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
 
-import { runGame, proceed} from './lib.js'
+import { 
+  renderMap, 
+  allShuffled, 
+  countryClicked
+} from './lib.js'
 
+document.addEventListener('DOMContentLoaded', () => {
 
+  const game = document.querySelector('#container')
+  const countryDisplay = document.querySelector('#country-display')
+  const feedback = document.querySelector('#feedback')
 
-document.addEventListener('DOMContentLoaded', function () {
-  // window.setTimeout(function(){
-  //   console.log(theChart, extracted)
-  runGame()
-  // }, 500)
+  countryDisplay.innerHTML = ''
+  feedback.innerHTML = ''
+  
+  let theMap = renderMap()
+  const theDeck = allShuffled()
+  var ci = 0
+  let up = theDeck[ci]
+  
+  countryDisplay.innerHTML = up.name
+
+  game.addEventListener('click', e => {
+    if (countryClicked(e)["hc-key"] == up.hc ){
+      // update the map
+      console.log(countryClicked(e))
+
+      // load up next country
+      ci++
+      up = theDeck[ci]
+      countryDisplay.innerHTML = up.name
+      feedback.innerHTML = '😀'
+    }
+
+    else {
+      feedback.innerHTML = '😞'
+    }
+
+    // ok this will consistently recolor the faroe islands, so we need to map to correct data item to update
+    console.log(theMap.series[0].data[0].update({color: '#bada55'}))
+    //theMap.redraw()
+    
+  });
 }, false);
 
-// var extracted = [] // radnomize this
-// var currentCountryIndex = 0;
-// var theChart = null
-
-// runGame()
 
   
 
