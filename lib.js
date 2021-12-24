d9a95417ef47cede2a2a57b57819bac5d1582ee9
Joseph Bacal <bacalj@gmail.com>
2021-12-24 13:38:20 -0500

    maybe this will help

--- lib.js
+++ lib.js
@@ -210,7 +210,8 @@ export function renderMap(){
         chart: {
             map: 'custom/world-eckert3-highres',
             backgroundColor: "#1C00ff00",
-            animation: false
+            animation: false,
+            styledMode: false
         },
 
         tooltip: {
